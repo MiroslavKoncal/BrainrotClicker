f1422ddec9e98f599d8bc1e6f67785d94e04dbaf
MiroslavKoncal <miroslavkoncal8@gmail.com>
2025-05-27 19:10:29 +0200

    komentáře 🙂

--- index2.js
+++ index2.js
@@ -44,6 +44,7 @@ const upgrades =
     };
 
 
+// dictionary ve kterém se uchvávají všechny zvířátka, jejich cena, nasobič ceny atd.
 const animals =
     {
         tung:
@@ -59,6 +60,8 @@ const animals =
             }
     };
 
+
+// uchovává data do localstorage aby se při jakémkoliv refreshy vypnutí a podobně všechna data uživateli neresetovali
 function saveData ()
 {
     if (!isNaN(score))
@@ -82,11 +85,13 @@ function saveData ()
     localStorage.setItem("critValueValue", JSON.stringify(upgrades["critValue"].value));
 }
 
-
+// vyčistí všechna uživatelská data
 function clearData(clearPrestige = false)
 {
+
     localStorage.clear();
 
+    // po vyčistění dat přiřadí všem proměnným zpět jejich základní hodnoty
     score = 0;
 
     upgrades["crit"].value = 0;
@@ -101,32 +106,31 @@ function clearData(clearPrestige = false)
     animals["tung"].count = 0;
     animals["tung"].price = 10;
 
+    // pokud do funkce vejde true přenastavý se i hodnoty pro pestige
     if (clearPrestige)
     {
         upgrades["prestige"].price = 100_000;
         upgrades["prestige"].value = 1
     }
-
+    // obnoví obraz aby zoobrazoval změněné hodnoty
     updateScoreDisplay();
 }
 
 
 function buyAnimal(animalKey)
 {
+    // uloží si zvíře a jeho data za pomocí kliče zadaného v  parametrech
     let animal = animals[animalKey];
 
     if (score >= animal.price)
     {
+        // zmení cenu, vezme skóré a zahraje zvuk daného zvířete pokud bylo skóré větší než cena
         score -= animal.price;
         animal.count += 1;
         animal.price = priceChange(animal.price, animal.priceMultiplier);
         animal.sound.play();
-
+        // obnoví obraz aby zoobrazoval změněné hodnoty
         updateScoreDisplay();
-    }
-    else
-    {
-        console.log(`Není dost peněz na koupi ${animal.name}. Potřeba: ${animal.price}, nyní: ${score}`);
     }
 }
 
@@ -134,6 +138,7 @@ function buyAnimal(animalKey)
 function getTotalCPS()
 {
     let cps = 0;
+    // postupně spočítá kolik je celková hodnota kolik zvířata vydělají za sekundu
     for (let key in animals)
     {
         cps += animals[key].count * animals[key].scoreValue * upgrades["prestige"].value;
@@ -144,15 +149,19 @@ function getTotalCPS()
 
 function playerClicker(crit = false)
 {
+    // pokud do funkce vejde parametr true:
     if (crit)
     {
         let clicks = upgrades["click"].value * upgrades["critValue"].value * upgrades["prestige"].value;
+        // secte hodnotu clicks s hodnodnotou kterou vrací funkce getTotalCPS a vypíše jí na obrazovku
         cps_label.innerHTML = getTotalCPS() + clicks;
         return clicks;
     }
+    // jinak:
     else
     {
         let clicks = upgrades["click"].value * upgrades["prestige"].value
+        // secte hodnotu clicks s hodnodnotou kterou vrací funkce getTotalCPS a vypíše jí na obrazovku
         cps_label.innerHTML = getTotalCPS() + clicks;
         return clicks;
     }
@@ -186,15 +195,17 @@ function upgrade (key)
     {
         if (key === "crit")
         {
+            // vylepší se šance na kritický zásah
             upgrades[key].value += upgrades["crit"].valueMultiplier;
         }
         else
         {
+            // zvětší hodnoty pro vylepšení které užívatel vybral
             upgrades[key].value = Math.round(upgrades[key].value * upgrades[key].valueMultiplier);
         }
+        // odebere skore, změní cenu a vypíše na obrazovku
         score -= upgrades[key].price;
         upgrades[key].price = priceChange(upgrades[key].price, upgrades[key].priceMultiplier);
-        console.log(`Nová cena ${upgrades[key].price}, nova value ${upgrades[key].value}`);
     }
     updateScoreDisplay();
 }
@@ -204,20 +215,17 @@ function prestige ()
 {
     if (score >= upgrades["prestige"].price)
     {
+        // kdyz uzivatel koupi prestige resetuje vsechna data a zvetsi hodnotu prestige o 10 nasledne zmeni cenu
         upgrades["prestige"].value *= 10;
         upgrades["prestige"].price = priceChange(upgrades["prestige"].price, upgrades["prestige"].priceMultiplier);
-        console.log(`Nová cena ${upgrades["prestige"].price}, nova value ${upgrades["prestige"].value}`);
         clearData();
-    }
-    else
-    {
-        console.log(`cena ${upgrades["prestige"].price}`);
     }
 }
 
 
 function priceChange (price, multiplier)
 {
+    // vratí zaokrouhlenou hodnotu nasobku ceny a nasobitele ceny
     return Math.round(price * multiplier);
 }
 
@@ -226,6 +234,7 @@ function addScoreFromAnimals ()
 {
     for (let key in animals)
     {
+        // prochází postupně klíči všech zvířat a vždy ke skoré pričte jejich score vynásobené jejich počtem a prestige
         let animal = animals[key];
         if (animal.count > 0)
         {
@@ -238,17 +247,22 @@ function addScoreFromAnimals ()
 
 function updateScoreDisplay ()
 {
+    // nastavi hodnotu pro skore do html
     score_label.innerHTML = score;
+
+    // postupně projde všechny zvířata a vypíše do html hodnoty
     for (let animal in animals)
     {
         animals[animal].priceLabel.innerHTML = animals[animal].price;
         animals[animal].countLabel.innerHTML = animals[animal].count;
     }
+    // postupně projde všechny vylepšení a vypíše do html hodnoty
     for (let upgrade in upgrades)
     {
         upgrades[upgrade].priceLabel.innerHTML = upgrades[upgrade].price;
         upgrades[upgrade].valueLabel.innerHTML = upgrades[upgrade].value;
     }
+    // uloží hodnoty
     saveData();
 }
 
@@ -258,6 +272,7 @@ const audio = new Audio("./media/theme_music.mp3");
 audio.loop = true;
 let isPlaying = false;
 
+// po stlačení tlačítka se buď zapne pisnička nebo vypne podle stavu
 document.getElementById("playButton").addEventListener("click", () => {
     if (isPlaying)
     {
@@ -274,10 +289,17 @@ document.getElementById("playButton").addEventListener("click", () => {
     isPlaying = !isPlaying;
 });
 
+
+//každou sekundu zavolá funkci a přidelí html elementu novou hodnotu
 setInterval(() => {document.getElementById("cps").innerHTML = getTotalCPS();}, 1000);
 
+
+// každou sekundu přidá score ze zvířat
 setInterval(() => addScoreFromAnimals(), 1000);
 
-window.onload = () => {
+
+// po zapnutí stránky se obnoví co je na obrazovce
+window.onload = () =>
+{
     updateScoreDisplay();
 };
